5ef691af272edfc152de88e74d1cec76a71d2572
Craig McLean <craig.mclean39@gmail.com>
2021-09-26 17:11:50 -0700

    Add edit and delete buttons
    Added edit and delete buttons to the individual work experiences and
    education histories. Added data-html2canvas-ignore datat attribute around
    them so they aren't rendered in the final resume output.

--- src/components/ResumeEducationExperience.js
+++ src/components/ResumeEducationExperience.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const ResumeEducationExperience = (props) => {
   const { educationExperience } = props;
@@ -11,9 +13,30 @@ const ResumeEducationExperience = (props) => {
         mt: 0,
       }}
     >
-      <Typography variant="subtitle2" sx={{ mb: -0.5 }} color="info.dark">
-        {educationExperience._educationTitle}
-      </Typography>
+      <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+        <Typography variant="subtitle2" sx={{ mb: -0.5 }} color="info.dark">
+          {educationExperience._educationTitle}
+        </Typography>
+        <Box data-html2canvas-ignore="true" sx={{ lineHeight: 0 }}>
+          <IconButton
+            aria-label="edit"
+            sx={{ width: "16px", height: "16px", ml: 1 }}
+          >
+            <EditIcon
+              sx={{ color: "common.black", width: "16px", height: "16px" }}
+            />
+          </IconButton>
+          <IconButton
+            aria-label="delete"
+            sx={{ width: "16px", height: "16px", mx: 0.5 }}
+          >
+            <DeleteIcon
+              sx={{ color: "common.black", width: "16px", height: "16px" }}
+            />
+          </IconButton>
+        </Box>
+      </Box>
+
       <Typography variant="caption">{educationExperience._orgName}</Typography>
       <Box
         sx={{
--- src/components/ResumeWorkExperience.js
+++ src/components/ResumeWorkExperience.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const ResumeWorkExperience = (props) => {
   const { workExperience } = props;
@@ -11,9 +13,29 @@ const ResumeWorkExperience = (props) => {
         mt: 0,
       }}
     >
-      <Typography variant="subtitle2" color="info.dark" sx={{ mb: -0.5 }}>
-        {workExperience._jobTitle}
-      </Typography>
+      <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+        <Typography variant="subtitle2" color="info.dark" sx={{ mb: -0.5 }}>
+          {workExperience._jobTitle}
+        </Typography>
+        <Box data-html2canvas-ignore="true" sx={{ lineHeight: 0 }}>
+          <IconButton
+            aria-label="edit"
+            sx={{ width: "16px", height: "16px", ml: 1 }}
+          >
+            <EditIcon
+              sx={{ color: "common.black", width: "16px", height: "16px" }}
+            />
+          </IconButton>
+          <IconButton
+            aria-label="delete"
+            sx={{ width: "16px", height: "16px", mx: 0.5 }}
+          >
+            <DeleteIcon
+              sx={{ color: "common.black", width: "16px", height: "16px" }}
+            />
+          </IconButton>
+        </Box>
+      </Box>
       <Typography variant="caption">{workExperience._orgName}</Typography>
       <Box
         sx={{
